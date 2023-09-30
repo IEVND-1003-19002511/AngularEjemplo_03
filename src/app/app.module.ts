b906eb39e702b99932f61f3f7c0978caa7fb551f
Mauricio <salvameza543@gmail.com>
2023-09-29 21:14:49 -0600

    SMME 29-09-23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
 
 import { AppComponent } from './app.component';
 import { ProcuctoListComponent } from './productos/procucto-list/procucto-list.component';
 import { ProductoFilterPipe } from './productos/producto-filter.pipe';
 import { OperaBasComponent } from './calculos/opera-bas/opera-bas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Suma2Component } from './calculos/suma2/suma2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProcuctoListComponent,
     ProductoFilterPipe,
-    OperaBasComponent
+    OperaBasComponent,
+    Suma2Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
